1b403f92bec889e6029e39263a1694720d6465a7
partemen <partemen@gmail.com>
2021-12-30 12:39:27 -0500

    added table with routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import data from './data';
 
-const App = () => (
+const App = () => {
+  console.log(data)
+  return (
   <div className="app">
-  <header className="header">
-    <h1 className="title">Airline Routes</h1>
-  </header>
-  <section>
-    <p>
-      Welcome to the app!
-    </p>
-  </section>
-</div>
-)
+    <header className="header">
+      <h1 className="title">Airline Routes</h1>
+    </header>
+    <section>
+      <p>
+        Welcome to the app!
+      </p>
+    </section>
+    <div>
+      <table>
+        <tbody>
+          {data.routes.map((route, idx) =>
+            <tr key={idx}>
+              <td>{route.airline}</td>
+              <td>{route.src}</td>
+              <td>{route.dest}</td>
+            </tr>)}
+        </tbody>
+      </table>
+    </div>
+  </div>
+  )
+}
 
 export default App;
